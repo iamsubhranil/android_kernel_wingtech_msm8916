9610f9d25c1e3064d44c6bbb3f8f8a3a0b0a7771
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2016-10-24 21:53:33 +0530

    build_starter: Ask for confirmation before creating zip

--- build_starter.sh
+++ build_starter.sh
@@ -14,7 +14,7 @@ read -p "Do you want to rebuild all? " rebuild
 case $rebuild in
 	[Yy]* ) 
 	echo "Cleaning previously compiled files..";
-	make clean;
+	make clean && make mrproper;
 esac
 read -p "Do you want to overwrite present configuration with the default one? " choice
 case $choice in
@@ -29,5 +29,11 @@ echo "Switching to dtbTool directory.."
 cd $DTBTOOL_DIR
 echo "Executing dtbToolCM.."
 ./dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
-echo "Kernel compiled successfully!"
-bash $KERNEL_DIR/zipping.sh
+echo "Kernel compilation complete!"
+echo "Check above for any errors.."
+read -p "Do you want to create a flashable zip? " zip
+case $zip in
+	[Yy]* )
+	bash $KERNEL_DIR/zipping.sh
+esac
+echo "Process completed.."
